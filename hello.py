d9631c0956e61ea7aacf5a52f12c1937e337e1de
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-04-14 20:26:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ logging.basicConfig(
 )
 
 # Race-specific hashtag
-RACE_HASHTAG = "BahrainGP"
+RACE_HASHTAG = "SaudiArabianGP"
 
 # Update credentials section to use environment variables
 REDDIT_CLIENT_ID = os.environ.get("REDDIT_CLIENT_ID")
